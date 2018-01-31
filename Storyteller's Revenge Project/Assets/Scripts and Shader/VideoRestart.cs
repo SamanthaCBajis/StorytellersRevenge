5cb34a7996872ffbc5d099f0f67cee9d63cb6201
Samantha Bajis <Sammi.Bajis@gmail.com>
2018-01-31 18:37:57 -0500

    First Commit
    Storyteller's Revenge right from desktop-first commit.

---
+++ Storyteller's Revenge Project/Assets/Scripts and Shader/VideoRestart.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class VideoRestart : MonoBehaviour {
+	private UnityEngine.Video.VideoPlayer videoPlayer;
+	public GameObject restartButton;
+
+	void Start () {
+		videoPlayer = GetComponent<UnityEngine.Video.VideoPlayer> ();
+	}
+		
+		public void Stop()
+	{
+
+		if (videoPlayer.isPlaying)
+			videoPlayer.frame = 0;
+	}
+		
+	public void Restart()
+	{
+		videoPlayer.Play();
+	}
+}
